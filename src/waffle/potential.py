dd41643cbf8af15d06e23269a5b62cf5a5554024
Buck Baskin <mobile.wbaskin@gmail.com>
2016-04-10 14:05:20 -0400

    More outining for the potential

--- src/waffle/potential.py
+++ src/waffle/potential.py
@@ -1,6 +1,36 @@
 from geometry_msgs.msg import Pose, Quaternion
 from sensor_msgs.msg import LaserScan
 
+class NaivePotential(Potential):
+    '''
+    An implementation of the Potential class/algorithm that assumes the only
+    obstacles that exist are those it can see.
+
+    This will likely fail without a 360 degree scan, because it will only see
+    obstacles in front of it, which would "push" it backwards.
+    '''
+    pass
+
+class SavingPotential(Potential):
+    '''
+    An implementation of the Potential class/algorithm that keeps track of all
+    of the points that it has seen a laser return from.
+
+    This will likely fail with dynamic obstacles, because it will still see old
+    obstacles even if a laser scan has shown the obstacle to have moved. With
+    static obstacles, this should outperform the NaivePotential.
+    '''
+    pass
+
+class ImprovedPotential(Potential):
+    '''
+    An attempt to improve the performance of the potential field algorithm,
+    (likely Saving) by trying to avoid local minima, and make other changes to
+    improve its completeness, or at least the variety of geometries that it can 
+    handle without getting caught in local minima.
+    '''
+    pass
+
 class Potential(object):
     def __init__(self):
         self.goal = Pose()
